32fb7894a199b4bf79a6e748386003e8757c3200
Nicolas Puegher <nicopuegher@gmail.com>
2021-09-22 20:34:56 +0200

    add contact component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './Main.module.css';
 import Background from './Background/Background';
 // import Welcome from './Welcome/Welcome';
-import Menu from './Menu/Menu';
+// import Menu from './Menu/Menu';
+import Contact from './Contact/Contact';
 
 const Main = () => (
   <main>
     <Background />
     {/* <Welcome /> */}
-    <Menu />
+    {/* <Menu /> */}
+    <Contact />
   </main>
 );
 
